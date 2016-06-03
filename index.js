5149196f4bf5e8ff63f78bf5d1931fef7bb6dd77
Colin Lee <Colin Lee>
2016-06-03 14:57:36 -0300

    All messages are now stored in the DB and the "chat" display is way less horrible.

--- index.js
+++ index.js
@@ -33,9 +33,35 @@ var fakebook = https.createServer({
 });
 
 app.post('/fakebook', function(req, res){
-  console.log('Received request from chat server');
-  console.log(req.body);
+  console.log("req.body = ", req.body);
+  var msg = {message: req.body.message.text, user_id: req.body.recipient.id};
   io.emit('chat message', req.body.message.text);
+  mongo.userCollection.findOne({user_id:msg.user_id}, function(err,user) {
+
+    if (err) {
+      console.log("Something went wrong finding user.");
+    }
+
+    if (user == null) {
+      // create a new record
+      user = mongo.userCollection.insert({user_id:msg.user_id}, function(err,result){
+        if (err) {
+          console.log("Unable to create user:", err);
+        } else {
+        }
+      });
+    }
+
+    mongo.messageCollection.insert({user: user, message: msg.message, time:Date.now(), from: false}, function(err,res){
+      if (err)
+      {
+        console.log("Unable to insert message into db:", err);
+      }
+      else
+      {
+      }
+    });
+  });
   res.json({'recipient_id':'bonk', 'message_id':'mid.bonk1234'});
   res.end();
 });
@@ -44,7 +70,6 @@ io.on('connection', function(socket) {
   console.log('a user connected');
 
   socket.on('chat message', function(msg){
-    console.log("Searching for user with id ", msg.user_id);
     mongo.userCollection.findOne({user_id:msg.user_id}, function(err,user) {
 
       if (err) {
@@ -57,19 +82,16 @@ io.on('connection', function(socket) {
           if (err) {
             console.log("Unable to create user:", err);
           } else {
-            console.log("Created user: ", result);
           }
         });
       }
-
-      console.log("Found user ", user);
 
       var formData = {
         "object":"page",
         "entry":[
           {
             "id":"PAGE_ID",
-            "time":123456789,
+            "time":Date.now(),
             "messaging":[
               {
                 "sender":{
@@ -101,29 +123,25 @@ io.on('connection', function(socket) {
         }
       };
 
-      var msg_rec = mongo.messageCollection.insert({user: user, message: formData.entry[0].messaging}, function(err,res){
+      var msg_rec = mongo.messageCollection.insert({user: user, message: msg.message, time:Date.now(), from: true}, function(err,res){
         if (err)
         {
           console.log("Unable to insert message into db:", err);
         }
         else
         {
-          console.log("Inserted message into db:", res);
         }
       });
 
       var req = http.request(options, function(res)
       {
         console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
         res.setEncoding('utf8');
         res.on('data', function(chunk)
         {
-          console.log('BODY: ' + chunk);
         });
         res.on('end', function()
         {
-          console.log('No more data in response.')
         })
       });
 
@@ -135,7 +153,6 @@ io.on('connection', function(socket) {
       console.log('Received message: ', msg);
       var postData = JSON.stringify(formData);
       // write data to request body
-      console.log('Sending data (length: ' + postData.length + '): ' + postData);
       req.write(postData);
       req.end();
 
